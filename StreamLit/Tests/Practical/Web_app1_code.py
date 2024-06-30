a68560937165c91bcd5d8e5b11eec874f3f13849
Adal <imadal525@gmail.com>
2024-06-30 23:17:52 +0530

    Add files via upload

---
+++ StreamLit/Tests/Practical/Web_app1_code.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("This is Streamlit")
+st.button("Welcome")
+st.image("OIP.jpeg")
+st.caption("This is python script in Streamlit")
+st.slider("how much years of experience do you have in python",0.0,5.0,0.3)
